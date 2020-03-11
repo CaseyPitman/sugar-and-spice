dc4d45e5b62ee38a92557f2e8f581738eaafce8d
Casey The Great <casey@caseypitman.com>
2020-03-11 08:57:42 -0500

    Added js to collapse mobile navbar after selection

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//jQuery
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+//Collapse Navbar after Selection on Mobile
+ $('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+});
