075a860c5627ee78942b6a79526e5b851e989f1d
Casey The Great <casey@caseypitman.com>
2020-04-03 09:13:37 -0500

    menu fixed for responsiveness

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 //jQuery
 $(document).ready(function(){
 
-
-
-
-
-
-
-
-
 //Collapse Navbar after Selection on Mobile
  $('.collapse-bar').on('click', function(){
     $('.navbar-collapse').collapse('hide');
